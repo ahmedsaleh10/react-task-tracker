9efd22dd183ff4d69c3d36f9f2946332d3624a70
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-21 12:30:43 +0300

    update the functionallity of handlers.

--- src/App.js
+++ src/App.js
@@ -1,81 +1,48 @@
 import AddTask from "./components/AddTask";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import { useState, useEffect } from "react"
+import { useState } from "react";
 import Footer from "./components/Footer";
-
+import { taskData } from "./mock/db";
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState(taskData);
 
-  const[showAddTask,setShowAddTask] = useState(false)
-  const[tasks,setTasks]=useState([])
+  //Add Task
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
 
-  useEffect ( () => {
-    const getTasks = async () => {
-      const tasksFromServer = await fetchTasks()
-      setTasks(tasksFromServer)
-    }
-    getTasks()
-  },[])
+  //delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
-//Featching method
+  // Add toggle reminder if double clicked on any task
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
-const fetchTasks = async () => {
-  const responce = await fetch('http://localhost:5000/tasks')
-  const data = await responce.json()
- 
-  return data
-}
-
-const fetchTask = async (id) => {
-  const responce = await fetch(`http://localhost:5000/tasks/${id}`)
-  const data = await responce.json()
- 
-  return data
-}
-
-//Add Task
-const addTask = async (task)=> {
-  const res = await fetch(`http://localhost:5000/tasks`, {method : 'POST' , headers:{'Content-type' : "application/json"} , body: JSON.stringify(task)})
-  const data = await res.json()
-
-  setTasks ([...tasks,data])
-}
-
-//delete task
-const deleteTask = async (id)=> {
-  
-  await fetch(`http://localhost:5000/tasks/${id}`, {method : 'Delete'})
-  setTasks(tasks.filter((task)=> task.id !== id))
-}
-
-// Add toggle reminder if double clicked on any task
-const toggleReminder = async (id)=> {
-  const taskToToggle = await fetchTask(id)
-  const updTask = {...taskToToggle , reminder : !taskToToggle.reminder}
-  const res = await fetch(`http://localhost:5000/tasks/${id}`, {method : 'PUT' , headers:{'Content-type' : "application/json"} , body: JSON.stringify(updTask)})
-
-  const data = await res.json()
-
-  setTasks(
-    tasks.map((task)=> 
-    task.id === id ? {...task, reminder: data.reminder} : task
-    )
-  )
-
-}
-
-const onAdd = () => {
-  setShowAddTask(!showAddTask)
-}
+  const onAdd = () => {
+    setShowAddTask(!showAddTask);
+  };
 
   return (
     <div className="container">
-       <Header title={"Task Tracker"} onClick={onAdd} showAdd={showAddTask}/>
-       {showAddTask && <AddTask onAdd={addTask} />}
-       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No Tasks To Show"}
-       <Footer />
+      <Header title={"Task Tracker"} onClick={onAdd} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
+      <Footer />
     </div>
   );
-  }
+}
 
 export default App;
